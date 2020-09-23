54b234e62e4f45b4e29ca3e561d85aaa54e7cb50
woodyZootopia <piequalsabout314159@gmail.com>
2020-09-24 05:31:56 +0900

    日本語のソースコードコメントを移植

--- listings/ch19-advanced-features/listing-19-31/hello_macro/hello_macro_derive/src/lib.rs
+++ listings/ch19-advanced-features/listing-19-31/hello_macro/hello_macro_derive/src/lib.rs
@@ -6,10 +6,12 @@ use syn;
 
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // 操作可能な構文木としてのRustコードの表現を構築する
     // Construct a representation of Rust code as a syntax tree
     // that we can manipulate
     let ast = syn::parse(input).unwrap();
 
+    // トレイトの実装内容を構築
     // Build the trait implementation
     impl_hello_macro(&ast)
 }
