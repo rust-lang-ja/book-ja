f5a36812232a1cae590bd9217b48e0f1dad76243
akinechan <weezer.and.you@gmail.com>
2022-09-22 10:47:53 +0900

    Fix typo: 重複していた ; を修正
    `;` が重複していたため修正

--- listings/ch03-common-programming-concepts/no-listing-01-variables-are-immutable/src/main.rs
+++ listings/ch03-common-programming-concepts/no-listing-01-variables-are-immutable/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let x = 5;
-    println!("The value of x is: {}", x);;     // xの値は{}です
+    println!("The value of x is: {}", x);     // xの値は{}です
     x = 6;
     println!("The value of x is: {}", x);
 }
